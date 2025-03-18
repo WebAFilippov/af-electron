41376a9614f4d8c7f5967914c64c262ddafdf234
AFilippov <64398851+WebAFilippov@users.noreply.github.com>
2025-03-19 03:02:32 +0700

    upd tray logic

--- src/main/ui/tray.ts
+++ src/main/ui/tray.ts
@@ -2,33 +2,47 @@ import { BrowserWindow, Menu, nativeImage, Tray } from 'electron'
 
 import icon from '../../../build/tray-48x48.png?asset'
 
+const toggleWindowVisibility = (window: BrowserWindow | null) => {
+  if (!window) return
+
+  if (window.isVisible()) {
+    window.hide()
+  } else {
+    if (window.isMinimized()) {
+      window.restore()
+    }
+    window.show()
+  }
+}
+
 export const createTray = (window: BrowserWindow) => {
   const tray = new Tray(nativeImage.createFromPath(icon))
 
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Развернуть/свернуть',
+      click: () => toggleWindowVisibility(window)
+    },
+    { type: 'separator' },
+    {
+      label: 'Выход',
+      role: 'quit'
+    }
+  ])
+
   tray.setToolTip('Harmonify')
-  tray.setContextMenu(
-    Menu.buildFromTemplate([
-      {
-        label: 'Развернуть/свернуть',
-        click: () => {
-          if (window) {
-            if (window.isVisible()) window.hide()
-            else window.show()
-          }
-        }
-      },
-      { type: 'separator' },
-      {
-        label: 'Выход',
-        role: 'quit'
-      }
-    ])
-  )
+  tray.setContextMenu(contextMenu)
+
   tray.on('click', () => {
-
+    toggleWindowVisibility(window)
   })
-  tray.on('double-click', () => {
 
+  tray.on('double-click', () => {
+    toggleWindowVisibility(window)
+  })
+
+  tray.on('right-click', () => {
+    tray.popUpContextMenu(contextMenu)
   })
 
   return tray
