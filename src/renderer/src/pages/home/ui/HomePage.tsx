a33cdbf9c4359025c1cdb0104612acf5e49890a0
AFilippov <64398851+WebAFilippov@users.noreply.github.com>
2025-03-27 21:38:58 +0700

    Revert "Удалил кнопку page/homepage" Удалили кнопку page/homepage
    This reverts commit 8f1b24ecad523fd88b7529fec74ad463c6273cbe.

--- src/renderer/src/pages/home/ui/HomePage.tsx
+++ src/renderer/src/pages/home/ui/HomePage.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 
 import { ROUTES } from '@shared/config/routing'
-import { Card, CardDescription, CardTitle } from '@shared/ui'
+import { Button, Card, CardDescription, CardTitle } from '@shared/ui'
 
 export const HomePage = () => {
   return (
