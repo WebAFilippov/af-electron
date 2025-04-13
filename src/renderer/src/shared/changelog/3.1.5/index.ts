bffc74d3827996ab3d4d3d93020d281a62b65f36
AFilippov <64398851+WebAFilippov@users.noreply.github.com>
2025-04-13 23:55:39 +0700

    up

---
+++ src/renderer/src/shared/changelog/3.1.5/index.ts
@@ -0,0 +1,6 @@
+export const v3_1_5 = {
+  version: '3.1.5',
+  date: '13-04-2025',
+  description: ['check'],
+  changelog: ['check',]
+}
--- src/renderer/src/shared/changelog/index.ts
+++ src/renderer/src/shared/changelog/index.ts
@@ -1,6 +1,7 @@
 import { v3_0_0 } from './3.0.0'
 import { v3_0_8 } from './3.0.8'
 import { v3_1_0 } from './3.1.0'
+import { v3_1_5 } from './3.1.5'
 
 type ChangelogItem = {
   version: string
@@ -9,4 +10,4 @@ type ChangelogItem = {
   changelog?: string[]
 }
 
-export const CHANGELOG: ChangelogItem[] = [v3_1_0, v3_0_8, v3_0_0]
+export const CHANGELOG: ChangelogItem[] = [v3_1_5, v3_1_0, v3_0_8, v3_0_0]
